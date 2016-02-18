f6820be7f1ed159839d0b0e437b4e0d67d44d7a9
Michael Dewey <deweym@rpi.edu>
2016-02-18 14:40:53 -0500

    Added test assets. Started work on player input/movement.

---
+++ Assets/player/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    KeyCode up_key = KeyCode.W;
+    KeyCode down_key = KeyCode.S;
+    KeyCode left_key = KeyCode.S;
+    KeyCode right_key = KeyCode.D;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(up_key)) rb.AddForce(new Vector2(0, 1));
+        if (Input.GetKeyDown(down_key)) rb.AddForce(new Vector2(0, -1));
+        if (Input.GetKeyDown(left_key)) rb.AddForce(new Vector2(-1,0));
+        if (Input.GetKeyDown(right_key)) rb.AddForce(new Vector2(1,0));
+
+
+	}
+}
--- Assets/player/PlayerMovement.cs
+++ Assets/player/PlayerMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
