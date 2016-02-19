7da856da6fefc57981299cad71a7c6375b4888dc
Philip Cioni <philcioni@gmail.com>
2016-02-19 01:37:12 -0500

    Added tween demo and modified player movement to use translate instead of addforce

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) { 
+			transform.Translate(Vector2.right * speed);
+		}
+		if (Input.GetKey(KeyCode.A)) { 
+			transform.Translate(Vector2.right * -speed);
+		}
+		if (Input.GetKey(KeyCode.S)) { 
+			transform.Translate(Vector2.up * -speed);
+		}
+		if (Input.GetKey(KeyCode.W)) { 
+			transform.Translate(Vector2.up * speed);
+		}
+	}
+}
