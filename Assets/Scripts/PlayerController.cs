afc6f2020347315560799f98aac361d0c5ce8f02
unknown <cionip@SA2502-PC01.win.rpi.edu>
2016-02-24 13:57:16 -0500

    Resolved merge conflict

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
     private string PID; // Joystick Number of a player
-
-	public float speed = 0.0f;
-    private Vector2 velocity;
+    
+    public float speed = 0.0f;
+    public Vector2 velocity;
     private Rigidbody2D rb;
+    int MAX_VELOCITY = 5;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         velocity = rb.velocity;
+
+        //ignore all collisions between players
+        List<GameObject> all_players = GameObject.FindGameObjectsWithTag("Player").ToList();
+        for (int i = 0; i < all_players.Count; i++)
+        {
+            print(all_players[i].name);
+            for (int j = 0; j < all_players.Count; j++)
+            {
+                if (all_players[i] != all_players[j])
+                {
+                    print("Canceling collisions between " + all_players[i] + " and " + all_players[j]);
+                    Physics2D.IgnoreCollision(all_players[i].GetComponent<Collider2D>(), all_players[j].GetComponent<Collider2D>());
+                }
+            }
+        }
+
     }
 
 	void Update () {
