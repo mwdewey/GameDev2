9413c088b6e68098c12b9c085eefd1a5bda1efd6
unknown <cionip@SA2502-PC01.win.rpi.edu>
2016-02-24 13:54:23 -0500

    Revamped PlayerController and changed inputManager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private string PID; // Joystick Number of a player
+
+	public float speed = 0.0f;
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        velocity = rb.velocity;
+    }
+
+	void Update () {
+        debugger();
+	}
+
+    void FixedUpdate()
+    {
+        velocity.x = Input.GetAxis("Joy1_LeftStickHorizontal") * speed;
+        velocity.y = Input.GetAxis("Joy1_LeftStickVertical") * speed; 
+        
+        rb.velocity = velocity;
+    }
+
+    void debugger()
+    {
+        if (Input.GetButtonDown("Joy1_MeleeAttack"))
+        {
+            print("melee attack");
+        }
+        if (Input.GetButtonDown("Joy1_RangedAttack"))
+        {
+            print("ranged attack");
+        }
+        if (Input.GetButtonDown("Joy1_SpecialAttack"))
+        {
+            print("special attack");
+        }
+        if (Input.GetButtonDown("Joy1_UltimateAttack"))
+        {
+            print("ultimate attack");
+        }
+    }
+
+}
