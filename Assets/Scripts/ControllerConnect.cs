1fd0a90c4ec70c5873d047fd20075d42215ec4f2
Michael Dewey <deweym@rpi.edu>
2016-02-24 15:24:57 -0500

    Updated controller ui

--- Assets/Scripts/ControllerConnect.cs
+++ Assets/Scripts/ControllerConnect.cs
@@ -10,36 +10,61 @@ public class ControllerConnect : MonoBehaviour
 {
     private Regex joystickRegex;
     private List<int> controllerIds;
+    private int controllerCount;
     private readonly int MAX_PLAYERS = 4;
     private readonly Color32 ACTIVE_COLOR = new Color32(56, 142, 60, 255);
     private readonly Color32 INACTIVE_COLOR = new Color32(211, 47, 47, 255);
 
-    private Vector2 icon_margin = new Vector2(20,20);
+    private List<Color32> playerColors;
+    private readonly Color32 P1_Color = new Color32(244, 67, 54, 255);
+    private readonly Color32 P2_Color = new Color32(33,150,243, 255);
+    private readonly Color32 P3_Color = new Color32(76, 175, 80, 255);
+    private readonly Color32 P4_Color = new Color32(255, 235, 59, 255);
 
-    public GameObject charSelectIcon_object;
-    private List<GameObject> charSelectIconList;
-    public int numOfChars = 8;
 
-    public GameObject playerIcon_object;
-    private List<GameObject> playerIconList;
-    private float playerIconWidth = 132.5f;
+    private List<GameObject> csList;
+    private List<GameObject> pbList;
+    private List<GameObject> dbList;
+    private GameObject cb;
 
-    public GameObject descriptionBox_object;
-    private List<GameObject> descriptionBoxList;
+    private List<List<int>> characterSelections;
+    private List<int> playerSelections;
 
-    public GameObject continueButton_object;
-    private GameObject continueButtonInstance;
-    private float continueButtonHeight = 40;
+    private List<float> timeSinceLastMovement; 
 
 	// Use this for initialization
 	void Start ()
 	{
         joystickRegex = new Regex(@"Joystick([0-9]+)Button([0-9]+)");
         controllerIds = new List<int>();
+        controllerCount = Input.GetJoystickNames().Length;
 
-        charSelectIconList = new List<GameObject>();
-        playerIconList = new List<GameObject>();
-        descriptionBoxList = new List<GameObject>();
+        playerColors = new List<Color32>();
+        playerColors.Add(P1_Color);
+        playerColors.Add(P2_Color);
+        playerColors.Add(P3_Color);
+        playerColors.Add(P4_Color);
+
+        csList = new List<GameObject>();
+        for (var i = 1; i < 5; i++) csList.Add(transform.Find("CS " + i).gameObject);
+
+        pbList = new List<GameObject>();
+        for (var i = 1; i < 5; i++) pbList.Add(transform.Find("PB " + i).gameObject);
+
+        dbList = new List<GameObject>();
+        for (var i = 1; i < 5; i++) dbList.Add(transform.Find("DB " + i).gameObject);
+
+        cb = transform.Find("CB").gameObject;
+
+        characterSelections = new List<List<int>>();
+        for (var i = 0; i < 4; i++) characterSelections.Add(new List<int>());
+        for (var i = 0; i < controllerCount; i++) characterSelections[0].Add(i);
+
+        playerSelections = new List<int>();
+        for (var i = 0; i < controllerCount; i++) playerSelections.Add(0);
+
+        timeSinceLastMovement = new List<float>();
+        for (var i = 0; i < controllerCount; i++) timeSinceLastMovement.Add(0);
 
         generateCharacterUI();
 	    generatePlayerUI();
@@ -51,7 +76,8 @@ public class ControllerConnect : MonoBehaviour
 	void Update ()
 	{
 
-	    DetectControllers();
+	    detectControllers();
+        moveSelections();
 
         if (Input.GetKeyDown(KeyCode.A)) generateCharacterUI();
 
@@ -59,80 +85,50 @@ public class ControllerConnect : MonoBehaviour
 
     void generateCharacterUI()
     {
-
-        foreach (GameObject g in charSelectIconList) Destroy(g);
-        charSelectIconList.Clear();
-
-        Vector2 win_dim = GetComponent<RectTransform>().sizeDelta;
-
-        for (var i = 0; i < numOfChars; i++)
+        for (var i = 0; i < 4; i++)
         {
-            GameObject uiIcon = Instantiate(charSelectIcon_object);
-            uiIcon.transform.parent = transform;
+            GameObject cs = csList[i];
+            List<int> currentSelected = characterSelections[i];
 
-            RectTransform rectTransform = uiIcon.GetComponent<RectTransform>();
-            float height = (win_dim.y - icon_margin.y * (numOfChars + 1)) / numOfChars;
-            rectTransform.sizeDelta = new Vector2(150, height);
-            rectTransform.anchoredPosition = new Vector2(95, -height * (i + 0.5f) - icon_margin.y * (i + 1));
-            rectTransform.localScale = new Vector3(1, 1, 1);
+            for (var i2 = 0; i2 < 4; i2++)
+            {
+                GameObject csIcon = cs.transform.Find("icon " + (i2+1)).gameObject;
+                Image csIconImage = csIcon.GetComponent<Image>();
 
-            charSelectIconList.Add(uiIcon);
+                if (currentSelected.Count > i2) csIconImage.color = playerColors[currentSelected[i2]];
+                else csIconImage.color = Color.white;
+            }
+
+
         }
-
     }
 
     void generatePlayerUI()
     {
-        //int playerCount = controllerIds.Count;
-        int playerCount = 4;
-        Vector2 win_dim = GetComponent<RectTransform>().sizeDelta;
-        float height = (win_dim.y - icon_margin.y*4 - continueButtonHeight)/2;
-
-        foreach (GameObject g in playerIconList) Destroy(g);
-        playerIconList.Clear();
-        foreach (GameObject g in descriptionBoxList) Destroy(g);
-        descriptionBoxList.Clear();
-
-        // create player selected character UIs and descriptions
-        for (var i = 0; i < playerCount; i++)
+        for (var i = 0; i < 4; i++)
         {
-            GameObject playerIcon = Instantiate(playerIcon_object);
-            playerIcon.transform.parent = transform;
+            GameObject pb = pbList[i];
+            GameObject db = dbList[i];
 
-            RectTransform rectTransform = playerIcon.GetComponent<RectTransform>();
-            rectTransform.sizeDelta = new Vector2(playerIconWidth, height);
-            rectTransform.anchoredPosition = new Vector2(190+playerIconWidth*(0.5f + i) + icon_margin.x*i, -icon_margin.y - height/2);
-            rectTransform.localScale = new Vector3(1, 1, 1);
+            Image pbImage = pb.GetComponent<Image>();
+            Image dbImage = db.GetComponent<Image>();
 
-            playerIconList.Add(playerIcon);
+            if (controllerIds.Count > i)
+            {
+                pbImage.color = playerColors[i];
+                dbImage.color = playerColors[i];
+            }
 
-            GameObject descriptionBox = Instantiate(descriptionBox_object);
-            descriptionBox.transform.parent = transform;
-
-            rectTransform = descriptionBox.GetComponent<RectTransform>();
-            rectTransform.sizeDelta = new Vector2(playerIconWidth, height);
-            rectTransform.anchoredPosition = new Vector2(190 + playerIconWidth * (0.5f + i) + icon_margin.x * i, -icon_margin.y * 2 - height * 1.5f);
-            rectTransform.localScale = new Vector3(1, 1, 1);
-
-            descriptionBoxList.Add(descriptionBox);
+            else {
+                pbImage.color = new Color32(33, 33, 33, 255);
+                dbImage.color = new Color32(33, 33, 33, 255);
+            }
 
         }
-
-        // create continue button
-        Destroy(continueButtonInstance);
-        continueButtonInstance = Instantiate(continueButton_object);
-        continueButtonInstance.transform.parent = transform;
-
-        RectTransform conTransform = continueButtonInstance.GetComponent<RectTransform>();
-        float continueButtonWidth = win_dim.x - 210;
-        conTransform.sizeDelta = new Vector2(continueButtonWidth, continueButtonHeight);
-        conTransform.anchoredPosition = new Vector2(190 + continueButtonWidth / 2, -win_dim.y + icon_margin.y + continueButtonHeight / 2);
-        conTransform.localScale = new Vector3(1, 1, 1);
-
     }
 
 
-    void DetectControllers()
+    void detectControllers()
     {
 
         if (Input.anyKeyDown)
@@ -212,6 +208,22 @@ public class ControllerConnect : MonoBehaviour
     {
 
         Debug.logger.Log("Count: " + controllerIds.Count);
+    }
+
+    void moveSelections()
+    {
+
+        for (var i = 0; i < 4; i++)
+        {
+
+            if (Input.GetButtonDown(" "))
+            {
+
+            }
+
+
+        }
+
     }
 
 
