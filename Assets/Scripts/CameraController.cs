707bf0f2081676fe7a39125373b148e6af0f30c2
Michael Dewey <deweym@rpi.edu>
2016-02-27 08:25:02 -0800

    Merge. Added split screen test scene.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject cameraObject;
+
+    private Camera camera;
+
+	// Use this for initialization
+	void Start () {
+
+        camera = cameraObject.GetComponent<Camera>();
+
+        camera.transform.SetParent(transform);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementTest : MonoBehaviour {
+
+    public int PID;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 velocity;
+
+    private KeyCode up;
+    private KeyCode down;
+    private KeyCode left;
+    private KeyCode right;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        velocity = new Vector2(0, 0);
+
+        if (PID == 0)
+        {
+            up = KeyCode.W;
+            down = KeyCode.S;
+            left = KeyCode.A;
+            right = KeyCode.D;
+        }
+
+        else if (PID == 1)
+        {
+            up = KeyCode.UpArrow;
+            down = KeyCode.DownArrow;
+            left = KeyCode.LeftArrow;
+            right = KeyCode.RightArrow;
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        velocity.x = 0;
+        velocity.y = 0;
+
+        if (Input.GetKey(up)) velocity.y += speed;
+        if (Input.GetKey(down)) velocity.y -= speed;
+        if (Input.GetKey(left)) velocity.x -= speed;
+        if (Input.GetKey(right)) velocity.x += speed;
+
+        rb.velocity = velocity;
+
+	}
+}
