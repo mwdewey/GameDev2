89d3a12bf81dfd2387175b6e873b8a7a7e5ebc0f
unknown <deweym@SA2502-PC02.win.rpi.edu>
2016-03-03 16:03:52 -0500

    Added movement animations

--- Assets/Scripts/PlayerMovementTest.cs
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -14,8 +14,12 @@ public class PlayerMovementTest : MonoBehaviour {
     private KeyCode left;
     private KeyCode right;
 
+    private Animator anim;
+
 	// Use this for initialization
 	void Start () {
+
+        anim = GetComponent<Animator>();
 
         rb = GetComponent<Rigidbody2D>();
         velocity = new Vector2(0, 0);
@@ -50,6 +54,49 @@ public class PlayerMovementTest : MonoBehaviour {
         if (Input.GetKey(right)) velocity.x += speed;
 
         rb.velocity = velocity;
+
+        // only handling cases with dual input
+        if (velocity.x != 0 && velocity.y != 0)
+        {
+            float angle = Mathf.Atan2(velocity.x, velocity.y) * Mathf.Rad2Deg;
+
+            // up
+            if (angle >= -45 && angle <= 45)
+            {
+                anim.SetFloat("delta_x", 0);
+                anim.SetFloat("delta_y", 1);
+            }
+
+            // down
+            else if (angle <= -135 || angle >= 135)
+            {
+                anim.SetFloat("delta_x", 0);
+                anim.SetFloat("delta_y", -1);
+            }
+
+            // left
+            if (angle < -45 && angle > -135)
+            {
+                anim.SetFloat("delta_x", -1);
+                anim.SetFloat("delta_y", 0);
+            }
+
+            // right
+            if (angle > 45 && angle < 135)
+            {
+                anim.SetFloat("delta_x", 1);
+                anim.SetFloat("delta_y", 0);
+            }
+
+            print(angle);
+        }
+
+        else
+        {
+            anim.SetFloat("delta_x", velocity.x);
+            anim.SetFloat("delta_y", velocity.y);
+        }
+
 
 	}
 }
