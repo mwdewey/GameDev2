4a93781215b298148ea5a0f89c241b3403232366
Philip Cioni <philcioni@gmail.com>
2016-03-28 22:48:51 -0400

    prepared controllers to be hooked together with the playerprefs

---
+++ Assets/Scripts/Characters/Ultimate Attacks/ultimateAttackController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ultimateAttackController : MonoBehaviour {
+    /*
+     * HOW TO USE THIS CONTROLLER:
+     * 
+     * 	  YOUR SCRIPT **MUST** HAVE A FUNCTION CALLED "ultimate()". This is what this script calls to
+     *      actually execute the attack.
+     * 
+     *    This script will be attached to a player, and will be called in playerController. When the 
+     *     "ultimate" button is pressed, this script will be invoked, check if the ultimate can happen,
+     *     and execute it appropriately.
+     */
+
+    public GameObject ultimatePrefab;
+	private int power = 0;
+	public MonoBehaviour Script;
+	private string character;
+
+
+	void Start () {
+		character = gameObject.GetComponent<PlayerController>().character;
+	}
+	
+	void Update () {
+	
+	}
+
+    public void handleUltimateInput() {
+        if (power >= 100) {
+			//gameObject.GetComponent (character + "_Ultimate").ultimate();
+        }
+    }
+
+}
