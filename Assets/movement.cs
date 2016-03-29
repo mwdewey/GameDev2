a80df76a9779c68eb7898fd25f024479b31283d7
Daniel Haynes <hayned2@gmail.com>
2016-03-28 22:02:13 -0400

    Adding in AI enemies to chase the players around. Still need to combine this with the current players.

---
+++ Assets/movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	Transform t;
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Transform> ();
+		health = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			t.position += new Vector3 (0.2f, 0.0f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			t.position += new Vector3 (0.0f, -0.2f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			t.position += new Vector3 (-0.2f, 0.0f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			t.position += new Vector3 (0.0f, 0.2f, 0.0f);
+		}
+
+	}
+}
