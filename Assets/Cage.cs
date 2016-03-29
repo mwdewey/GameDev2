126d8b896c5880e933c023fa1958a48896baaec4
Philip Cioni <philcioni@gmail.com>
2016-03-28 22:50:37 -0400

    Merge branch 'master' of https://github.com/mwdewey/GameDev2
    # Conflicts:
    #	Assets/Scripts/Characters/Ultimate Attacks.meta
    #	Assets/enemy.unity.meta
    #	Assets/font/Champagne.meta

---
+++ Assets/Cage.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cage : MonoBehaviour {
+
+	bool active;
+	bool locked;
+	float locked_timer;
+	movement victim;
+	public Sprite unlocked_sprite;
+
+	// Use this for initialization
+	void Start () {
+		active = true;
+		locked = false;
+		locked_timer = 10;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (!active) {
+			return;
+		}
+		if (other.tag == "PlayerObject") {
+			//Debug.Log ("MWAHAHA, YOU HAVE BEEN TRAPPED, MORTAL");
+			locked = true;
+			victim = other.GetComponent<movement> ();
+			victim.trapped = true;
+			other.transform.position = transform.position;
+			GetComponentsInChildren<SpriteRenderer>()[1].sprite = unlocked_sprite;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (locked && active) {
+			locked_timer -= Time.deltaTime;
+			if (locked_timer <= 0) {
+				//Debug.Log ("Okay, you can go now. I got bored");
+				locked = false;
+				active = false;
+				victim.trapped = false;
+			}
+		}
+	}
+}
