9490033e5aeeee595893a225d5a900df0723e1c8
Daniel Haynes <hayned2@gmail.com>
2016-03-29 21:12:03 -0400

    Added in a framework for picking up items and an example item, the Blue Shell Reference. Also removed some depricated files.

---
+++ Assets/Item.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Item : MonoBehaviour {
+
+	public SpriteRenderer ring;
+	public SpriteRenderer rendy;
+	public CircleCollider2D collidy;
+	public GameObject holder;
+	public bool activated;
+
+	// Use this for initialization
+	void Start () {
+		ring = GetComponentsInChildren<SpriteRenderer> () [1];
+		rendy = GetComponent<SpriteRenderer> ();
+		collidy = GetComponent<CircleCollider2D> ();
+		activated = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "PlayerObject") {
+			other.GetComponent<movement> ().item_list.Add (this);
+			ring.enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "PlayerObject") {
+			other.GetComponent<movement> ().item_list.Remove (this);
+			ring.enabled = false;
+		}
+	}
+
+	public void Picked_Up(){
+		ring.enabled = false;
+		rendy.enabled = false;
+		collidy.enabled = false;
+	}
+
+	public virtual void Activate(){
+		activated = true;
+	}
+
+	public void Drop(){
+		Debug.Log ("YOU HAVE DROPPED OUT A BLUE SHELL");
+		rendy.enabled = true;
+		collidy.enabled = true;
+		transform.position = holder.transform.position - new Vector3 (0.0f, 0.5f, 0.0f);
+		holder = null;
+	}
+}
--- Assets/movement.cs
+++ Assets/movement.cs
@@ -1,21 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class movement : MonoBehaviour {
 
 	Transform t;
 	public bool trapped;
-	public int health;
+	int health;
+	public List<Item> item_list;
+	Item held_item;
+	public int pid;
+	public int score;
+	public bool dummy;
 
 	// Use this for initialization
 	void Start () {
 		t = GetComponent<Transform> ();
 		health = 20;
+		held_item = null;
+		item_list = new List<Item> ();
+		//score = 0;
+	}
+
+	public void TakeDamage(int amount){
+		health -= amount;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (trapped) {
+		if (trapped || dummy) {
 			return;
 		}
 		if (Input.GetKey (KeyCode.D)) {
@@ -30,6 +43,22 @@ public class movement : MonoBehaviour {
 		if (Input.GetKey (KeyCode.W)) {
 			t.position += new Vector3 (0.0f, 0.2f, 0.0f);
 		}
-
+		if (Input.GetKeyDown (KeyCode.L)) {
+			if (held_item == null && item_list.Count > 0) {
+				held_item = item_list [0];
+				item_list.Remove (held_item);
+				held_item.Picked_Up ();
+				held_item.holder = gameObject;
+				Debug.Log ("Picked up item: " + held_item.name);
+			} 
+			else if (held_item != null){
+				held_item.Activate ();
+				held_item = null;
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.P) && held_item != null) {
+			held_item.Drop ();
+			held_item = null;
+		}
 	}
 }
