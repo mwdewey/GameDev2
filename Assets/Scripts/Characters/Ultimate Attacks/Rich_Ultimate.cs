7d80f471ce4ba4c8b1e830a8d9356cc2e44fc4df
Philip Cioni <philcioni@gmail.com>
2016-03-29 20:34:33 -0400

    I just want to pull, man

--- Assets/Scripts/Characters/Ultimate Attacks/Rich_Ultimate.cs
+++ Assets/Scripts/Characters/Ultimate Attacks/Rich_Ultimate.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ultimateAttack : MonoBehaviour {
+public class Rich_Ultimate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,9 +11,5 @@ public class ultimateAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void ultimate() {
-		Debug.Log ("ULTIMATE ATTACK!");
 	}
 }
