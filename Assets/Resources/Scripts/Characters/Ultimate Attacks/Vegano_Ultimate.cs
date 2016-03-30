5a6c6505103aadc32f0d825a3f6b121445ba8f2f
Daniel Haynes <hayned2@gmail.com>
2016-03-30 01:11:23 -0400

    VEGANO'S ULTIMATE ATTACK

--- Assets/Resources/Scripts/Characters/Ultimate Attacks/Vegano_Ultimate.cs
+++ Assets/Resources/Scripts/Characters/Ultimate Attacks/Vegano_Ultimate.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Vegano_Ultimate : MonoBehaviour {
 
+	GameObject minion;
+	Transform t;
+
 	// Use this for initialization
 	void Start () {
-	
+		minion = (GameObject) Resources.Load ("Prefabs/Enemy");
+		t = GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
@@ -14,6 +18,11 @@ public class Vegano_Ultimate : MonoBehaviour {
 	}
 
 	public void ultimate () {
-
+		Debug.Log ("I AM DOING MY ULTIMATE ATTACK NOW");
+		for (int x = 0; x < 3; x++){
+			GameObject temp = Instantiate (minion);
+			temp.transform.position = new Vector3 (t.position.x + 1 * Mathf.Cos (x * 2 * Mathf.PI / 3), t.position.y + 1 * Mathf.Sin (x * 2 * Mathf.PI / 3), t.position.x);
+			temp.GetComponent<EnemyBehavior> ().master = t;
+		}
 	}
 }
