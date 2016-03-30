458ebcc540a0a02eee924d2247ff6497db94ebd7
Michael Dewey <deweym@rpi.edu>
2016-03-30 01:39:27 -0400

    STUFF

---
+++ Assets/Resources/Scripts/GUI/UltimateBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UltimateBar : MonoBehaviour {
+
+    private PlayerController pc;
+    private Image meter;
+    private Color32 meterColor;
+    private float power;
+
+    public float min_power;
+    public float max_power;
+
+    private float icon_min_width = 0;
+    private float icon_max_width = 400;
+
+	// Use this for initialization
+	void Start () {
+        pc = transform.parent.parent.GetComponent<PlayerController>();
+        meter = transform.Find("meter").GetComponent<Image>();
+        meter.color = pc.playerColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 size = meter.rectTransform.sizeDelta;
+        size.x = pc.power / max_power * icon_max_width;
+        meter.rectTransform.sizeDelta = size;
+	}
+}
