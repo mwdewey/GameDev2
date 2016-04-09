e4c5e3d2145b3b3dd8c964cfa72f7f801c192ef5
Michael Dewey <deweym@rpi.edu>
2016-04-09 01:29:20 -0400

    Added end scene.

--- Assets/Resources/UI/PrintScores.cs
+++ Assets/Resources/UI/PrintScores.cs
@@ -1,19 +1,103 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class PrintScores : MonoBehaviour {
 
+    public RuntimeAnimatorController shifter_anim;
+    public RuntimeAnimatorController vegano_anim;
+    public RuntimeAnimatorController rich_anim;
+    public RuntimeAnimatorController missq_anim;
+
 	// Use this for initialization
 	void Start () {
-        int p1 = PlayerPrefs.GetInt("p" + 0 + "score");
-        int p2 = PlayerPrefs.GetInt("p" + 1 + "score");
-        int p3 = PlayerPrefs.GetInt("p" + 2 + "score");
-        int p4 = PlayerPrefs.GetInt("p" + 3 + "score");
+        for (var i = 0; i < 4; i++)
+        {
+            PlayerPrefs.SetInt("p" + (i + 1) + "score",i*4);
+            PlayerPrefs.SetInt("Player" + (i + 1) + "CharSelect",(int)CharCodes.Shifter);
+        }
 
-        string scoreText = "Score\nPlayer 1: " + p1 + "\nPlayer 2: " + p2 + "\nPlayer 3: " + p3 + "\nPlayer 4: " + p4;
+        List<PlayerEnd> players = new List<PlayerEnd>();
+        for(var i = 0; i < 4; i++){
+            int score = PlayerPrefs.GetInt("p" + (i + 1) + "score");
+            CharCodes charCode = (CharCodes) PlayerPrefs.GetInt("Player" + (i + 1) + "CharSelect");
 
-        GetComponent<Text>().text = scoreText;
+            players.Add(new PlayerEnd((i + 1), charCode, score));
+        }
+
+        List<PlayerEnd> sortedPlayers = players.OrderBy(p => p.score).ToList();
+        
+        List<Place> places = new List<Place>();
+        for (var i = 0; i < 4; i++)
+        {
+            GameObject place = transform.Find("Rank " + (i + 1)).gameObject;
+            Animator anim = place.GetComponent<Animator>();
+            Text text = place.transform.Find("Text").GetComponent<Text>();
+
+            places.Add(new Place(anim,text));
+        }
+
+        for (var i = 0; i < 4 ; i++)
+        {
+            PlayerEnd player = sortedPlayers[i];
+            Place place = places[3-i];
+
+            string placeText = "";
+            switch (3-i)
+            {
+                case 0: placeText = "1st"; break;
+                case 1: placeText = "2nd"; break;
+                case 2: placeText = "3rd"; break;
+                case 3: placeText = "4th"; break;
+            }
+
+            switch (player.charCode)
+            {
+                case CharCodes.MissQ: place.anim.runtimeAnimatorController = missq_anim; break;
+                case CharCodes.Shifter: place.anim.runtimeAnimatorController = shifter_anim; break;
+                case CharCodes.Vegano: place.anim.runtimeAnimatorController = vegano_anim; break;
+                case CharCodes.Rich: place.anim.runtimeAnimatorController = rich_anim; break;
+            }
+
+            string textOutput = placeText + " Player " + player.id + " Score " + player.score;
+
+            place.text.text = textOutput;
+
+        }
+
+        
+
+
 	}
 	
+}
+
+public class Place
+{
+    public Text text;
+    public Animator anim;
+
+    public Place(Animator a, Text txt) {
+
+        text = txt;
+        anim = a;
+
+    }
+}
+
+public class PlayerEnd
+{
+    public int id;
+    public CharCodes charCode;
+    public int score;
+
+    public PlayerEnd(int _id, CharCodes _c, int _score)
+    {
+        id = _id;
+        charCode = _c;
+        score = _score;
+    }
+
 }
