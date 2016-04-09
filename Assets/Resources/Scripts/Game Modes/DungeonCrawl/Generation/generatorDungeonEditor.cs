016b7cbd58ac5ea1852b1478eae941c6d01066eb
Philip Cioni <philcioni@gmail.com>
2016-04-08 21:22:01 -0400

    Tried to fix my uh-oh

--- Assets/Resources/Scripts/Game Modes/DungeonCrawl/Generation/generatorDungeonEditor.cs
+++ Assets/Resources/Scripts/Game Modes/DungeonCrawl/Generation/generatorDungeonEditor.cs
@@ -1,8 +1,9 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(dungeonGenerator))] 	
-public class DungeonizerEditor : Editor {
+[CustomEditor(typeof(generateDungeon))] 	
+public class generateDungeonEditor : Editor {
 	
 	public override void OnInspectorGUI () {
 		//Called whenever the inspector is drawn for this object.
@@ -10,18 +11,16 @@ public class DungeonizerEditor : Editor {
 		//This draws the default screen.  You don't need this if you want
 		//to start from scratch, but I use this when I'm just adding a button or
 		//some small addition and don't feel like recreating the whole inspector.
-		dungeonGenerator realscript = (dungeonGenerator)target;
+		generateDungeon realscript = (generateDungeon)target;
 
 		if(GUILayout.Button("Create Now")) {
 			//add everthing the button would do.
 			realscript.ClearOldDungeon(true);
 			realscript.Generate();
-			
 		}
-
-		if(GUILayout.Button("Clear")) {
-			//add everthing the button would do.
+		if(GUILayout.Button("Clear for Play")) {
 			realscript.ClearOldDungeon(true);
 		}
 	}
 }
+#endif
