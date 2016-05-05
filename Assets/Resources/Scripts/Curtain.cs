423c06715c2197b4dd28245243d1b44f31488f8a
Mike <Mike>
2016-05-05 14:52:24 -0400

    Changed ui icon and curtain to animations

--- Assets/Resources/Scripts/Curtain.cs
+++ Assets/Resources/Scripts/Curtain.cs
@@ -5,82 +5,55 @@ using UnityEngine.UI;
 
 public class Curtain : MonoBehaviour
 {
-    private Image curtainImage;
     public float transitionTime;
-    private float animTime;
 
+    private Image curtainImage;
+    private float animTime;
     public bool isRunning;
     private bool isOpening;
-
     private Color32 curtainColor;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start ()
 	{
-
-	    animTime = 0;
 	    isRunning = false;
         isOpening = true;
 
 	    curtainImage = GetComponent<Image>();
-
         curtainColor = new Color32(0, 0, 0, 0);
-
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    animateCurtain();
 
 	}
-
-    void animateCurtain()
-    {
-        if (isRunning)
-        {
-            if (isOpening)
-            {
-                float alpha = (transitionTime - animTime)/transitionTime*255;
-
-                if (alpha <= 0)
-                {
-                    isRunning = false;
-                    curtainColor.a = 0;
-                }
-                else curtainColor.a = (byte)Convert.ToInt32(alpha);
-            }
-
-            else
-            {
-                float alpha = animTime / transitionTime * 255;
-
-                if (alpha >= 255)
-                {
-                    isRunning = false;
-                    curtainColor.a = 255;
-                }
-                else curtainColor.a = (byte)Convert.ToInt32(alpha);
-            }
-
-            curtainImage.color = curtainColor;
-            animTime += Time.deltaTime;
-        }
-    }
 
     public void open()
     {
         isRunning = true;
-        animTime = 0;
         isOpening = true;
+        anim.SetTrigger("open");
+        anim.SetFloat("speed", transitionTime/2);
+        
+        Invoke("setISRunningFalse", transitionTime);
+    }
 
+    private void setISRunningFalse()
+    {
+        isRunning = false;
     }
 
     public void close()
     {
         isRunning = true;
-        animTime = 0;
         isOpening = false;
+        anim.SetTrigger("close");
+        anim.SetFloat("speed", transitionTime / 2);
+
+        Invoke("setISRunningFalse", transitionTime);
     }
 
     public void instantOpen()
